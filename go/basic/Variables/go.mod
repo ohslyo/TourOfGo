353f973828ac20f1323472e968b73c689cf58d4a
ohslyo <ohslyo@hotmail.co.jp>
2025-12-19 17:07:52 +0900

    Variables追加

---
+++ go/basic/Variables/go.mod
@@ -0,0 +1,3 @@
+module TourOfGo/go/basic/Variables
+
+go 1.25.1
---
+++ rust/basic/Variables/src/main.rs
@@ -0,0 +1,8 @@
+const C: bool = false;
+const PYTHON: bool = false;
+const JAVA: bool = false;
+
+fn main() {
+    let i: i32 = 0;
+    println!("{} {} {} {}", i, C, PYTHON, JAVA);
+}
