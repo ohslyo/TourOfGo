8fcc075ac6536b170e32090a1be61367c5cff400
ohslyo <ohslyo@hotmail.co.jp>
2026-06-26 12:57:45 +0900

    Short-variable-declarations

---
+++ go/basic/Short-variable-declarations/go.mod
@@ -0,0 +1,3 @@
+module TourOfGo/go/basic/Short-variable-declarations
+
+go 1.25.1
---
+++ rust/basic/Short-variable-declarations/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let i: i32 = 1;
+    let j: i32 = 2;
+    let k: i32 = 3;
+    let c: bool = true;
+    let python: bool = false;
+    let java: &str = "no!";
+    println!("{} {} {} {} {} {}", i, j, k, c, python, java);
+}
